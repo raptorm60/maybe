005a158f44d0f17b5dbec5484c1d731d8aea1256
Martin Baar <martin.baar@outlook.com>
2026-02-03 04:24:09 +0100

    Fixed currency exchange provider

--- app/models/exchange_rate/provided.rb
+++ app/models/exchange_rate/provided.rb
@@ -4,7 +4,7 @@ module ExchangeRate::Provided
   class_methods do
     def provider
       registry = Provider::Registry.for_concept(:exchange_rates)
-      registry.get_provider(:finnhub)
+      registry.providers.first
     end
 
     def find_or_fetch_rate(from:, to:, date: Date.current, cache: true)
